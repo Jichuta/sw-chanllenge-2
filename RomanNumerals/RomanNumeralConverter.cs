290a9dcf5c1aa3e94c64334db211c6a5d881773d
jichuta <ichuta.josoe@gmail.com>
2026-05-27 15:42:22 -0400

    feat: Convertion of roman to int number from 1 to 10

--- RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -89,7 +89,30 @@ namespace RomanNumerals
 
         public static int ConvertToInteger(string roman)
         {
-            return 1;
+            switch (roman)
+            {
+                case "I":
+                    return 1;
+                case "II":
+                    return 2;
+                case "III":
+                    return 3;
+                case "IV":
+                    return 4;
+                case "V":
+                    return 5;
+                case "VI":
+                    return 6;
+                case "VII":
+                    return 7;
+                case "VIII":
+                    return 8;
+                case "IX":
+                    return 9;
+                case "X":
+                    return 10;
+            }
+            throw new ArgumentOutOfRangeException(nameof(roman), "Invalid Roman numeral.");
         }
     }
 }
