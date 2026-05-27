18b53ad453d555b7e92b1c187313c5ba51c29cc5
jichuta <ichuta.josoe@gmail.com>
2026-05-27 10:15:04 -0400

    tdd: add convertion from number 1 to 10 and throw exception of argument out of range for numbers greter than 10

--- RomanNumerals.Tests/RomanNumeralConverterTest.cs
+++ RomanNumerals.Tests/RomanNumeralConverterTest.cs
@@ -4,15 +4,33 @@ namespace RomanNumerals.Tests
 {
     public class RomanNumeralConverterTest
     {
-        [Fact]
-
-        public void ConvertToRoman_ShouldReturnCorrectRomanNumeral()
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        public void ConvertToRoman_ShouldReturnCorrectRomanNumeral(int number, string expected)
         {
             // Act
-            var result = RomanNumeralConverter.ConvertToRoman(1);
+            var result = RomanNumeralConverter.ConvertToRoman(number);
 
             // Assert
-            Assert.Equal("I", result);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(11)]
+        [InlineData(15)]
+        public void ConvertToRoman_ShouldThrowExceptionForOutOfRangeNumbers(int number)
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => RomanNumeralConverter.ConvertToRoman(number));
         }
     }
 }
