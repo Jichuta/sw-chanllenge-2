6b4c1dde033db1026017ec6e8c74bff40baa1ca1
jichuta <ichuta.josoe@gmail.com>
2026-05-28 14:48:53 -0400

    refactor: refactor de function that convert an integer number into roman number

--- RomanNumerals/RomanNumeralConverter.cs
+++ RomanNumerals/RomanNumeralConverter.cs
@@ -5,11 +5,17 @@ namespace RomanNumerals
         public static readonly (string Roman, int Value)[] RomanNumerals = new (string, int)[]
         {
             ("M", 1000),
+            ("CM", 900),
             ("D", 500),
+            ("CD", 400),
             ("C", 100),
+            ("XC", 90),
             ("L", 50),
+            ("XL", 40),
             ("X", 10),
+            ("IX", 9),
             ("V", 5),
+            ("IV", 4),
             ("I", 1)
         };
 
@@ -41,82 +47,20 @@ namespace RomanNumerals
                 return string.Empty;
             }
 
-            if (number > 1000)
+            if (number < 0 || number > 1000)
             {
-                throw new ArgumentOutOfRangeException(nameof(number), "Number must be equal or less than 1000.");
+                throw new ArgumentOutOfRangeException(nameof(number), "Number must be greater than 0 or less than 1001.");
             }
 
-            if (number == 1000)
+            foreach (var (roman, value) in RomanNumerals)
             {
-                return "M";
+                if (number >= value)
+                {
+                    return roman + ConvertToRoman(number - value);
+                }
             }
-
-            if (number >= 1000)
-            {
-                return "M" + ConvertToRoman(number - 1000);
-            }
-
-            if (number >= 900)
-            {
-                return "CM" + ConvertToRoman(number - 900);
-            }
-
-            if (number >= 500)
-            {
-                return "D" + ConvertToRoman(number - 500);
-            }
-
-            if (number >= 400)
-            {
-                return "CD" + ConvertToRoman(number - 400);
-            }
-
-            if (number >= 100)
-            {
-                return "C" + ConvertToRoman(number - 100);
-            }
-
-            if (number >= 90)
-            {
-                return "XC" + ConvertToRoman(number - 90);
-            }
-
-            if (number >= 50)
-            {
-                return "L" + ConvertToRoman(number - 50);
-            }
-
-            if (number >= 40)
-            {
-                return "XL" + ConvertToRoman(number - 40);
-            }
-
-            if (number >= 10)
-            {
-                return "X" + ConvertToRoman(number - 10);
-            }
-
-            if (number >= 9)
-            {
-                return "IX" + ConvertToRoman(number - 9);
-            }
-
-            if (number >= 5)
-            {
-                return "V" + ConvertToRoman(number - 5);
-            }
-
-            if (number >= 4)
-            {
-                return "IV" + ConvertToRoman(number - 4);
-            }
-
-            if (number >= 1)
-            {
-                return "I" + ConvertToRoman(number - 1);
-            }
-
-            throw new ArgumentOutOfRangeException(nameof(number), "Number must be greater than or equal to 1.");
+            
+            throw new ArgumentOutOfRangeException(nameof(number), "Out of range number.");
         }
 
         public static int ConvertToInteger(string roman)
